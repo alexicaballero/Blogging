4135359dee1ac534390ee3f92e42eebc38a79721
Developer <devcicextuno@outlook.com>
2023-06-28 23:35:10 -0500

    first commit

---
+++ Blogging.WebApi/DataAccess/BloggingContext.cs
@@ -0,0 +1,11 @@
+﻿using Blogging.WebApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogging.WebApi.DataAccess
+{
+  public class BloggingContext : DbContext
+  {
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+  }
+}
---
+++ Blogging.WebApi/Program.cs
@@ -0,0 +1,36 @@
+using Blogging.WebApi.DataAccess;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var dbConnectionString = builder.Configuration.GetConnectionString("DbConnection");
+builder.Services.AddDbContext<BloggingContext>(options =>
+    options.UseSqlServer(dbConnectionString));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
